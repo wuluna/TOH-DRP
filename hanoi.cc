6e16c638dbe47116d75192fffce8c7107ee44ef4
Luna Wu <lunawu62@gmail.com>
2024-07-28 10:41:36 -0400

    changed to pass by reference

--- hanoi.cc
+++ hanoi.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 // moves the top disk from one stack to another
-void move(stack<int> a, stack<int> b) {
+void move(stack<int> &a, stack<int> &b) {
     if (a.empty() || (!b.empty() && b.top() < a.top())) {
         int top = b.top();
         b.pop();
@@ -17,35 +17,31 @@ void move(stack<int> a, stack<int> b) {
     }
 }
 
-void print_stack(stack<int> s) {
+void print_stack(stack<int> &s) {
     stack<int> scpy = s;
     while (!scpy.empty()) {
-        cout << scpy.top() << ", ";
+        cout << scpy.top() << " ";
         scpy.pop();
     }
     cout << endl;
 }
 
 // Iterative method
-void hanoi_it(int n, stack<int> source, stack<int> spare, stack<int> target) {
-    print_stack(source);
-    print_stack(spare);
-    print_stack(target);
-    cout << endl;
+void hanoi_it(int n, stack<int> &source, stack<int> &spare, stack<int> &target) {
     while (target.size() != n && spare.size() != n) {
         move(source, target);
         move(source, spare);
         move(target, spare);
 
-        print_stack(source);
-        print_stack(spare);
-        print_stack(target);
+        cout << "source: "; print_stack(source);
+        cout << "spare: "; print_stack(spare);
+        cout << "target: "; print_stack(target);
         cout << endl;
     }
 }
 
 // Recursive method
-void hanoi_rec(int n, stack<int> source, stack<int> target, stack<int> spare) {
+void hanoi_rec(int n, stack<int> &source, stack<int> &target, stack<int> &spare) {
     if (n == 1) {
         move(source, target);
     }
@@ -59,5 +55,13 @@ void hanoi_rec(int n, stack<int> source, stack<int> target, stack<int> spare) {
 int main(int argc, char *argv[]) {
     stack<int> source, spare, target;
     int n = stoi(argv[1]);
+    for (int i = n; i > 0; i-- ) {
+        source.push(i);
+    }
+
+    cout << "source: "; print_stack(source);
+    cout << "spare: "; print_stack(spare);
+    cout << "target: "; print_stack(target);
+    cout << endl;
     hanoi_it(n, source, spare, target);
 }
