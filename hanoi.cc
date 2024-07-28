c2e1d7d9b1873d3313427b28927a84b92fe2f937
Luna Wu <luna@Lunas-Air.lan>
2024-07-27 22:37:44 -0400

    wrote recurive method

--- hanoi.cc
+++ hanoi.cc
@@ -1,24 +1,43 @@
 #include <iostream>
 #include <stack>
 
+using namespace std;
+
 // moves the top disk from one stack to another
 void move(stack<int> a, stack<int> b) {
-    if (a.empty())
+    if (a.empty() || (!b.empty() && b.top() < a.top())) {
+        int top = b.top();
+        b.pop();
+        a.push(top);
+    }
+    else if (b.empty() || (!a.empty() && a.top() < b.top())) {
+        int top = a.top();
+        a.pop();
+        b.push(top);
+    }
+}
+
+void print_stack(stack<int> s) {
+    stack<int> scpy = s;
+    while (!scpy.empty()) {
+        cout << scpy.top() << ", ";
+        scpy.pop();
+    }
 }
 
 // Iterative method
+void hanoi_it(int n, stack<int> source, stack<int> target, stack<int> spare) {
 
+}
 
 // Recursive method
-
-
-void hanoi(int n, stack<int> source, stack<int> target, stack<int> spare) {
+void hanoi_rec(int n, stack<int> source, stack<int> target, stack<int> spare) {
     if (n == 1) {
         move(source, target);
     }
     else {
-        hanoi(n - 1, source, spare, target);
+        hanoi_rec(n - 1, source, spare, target);
         move(source, target);
-        hanoi(n - 1, spare, target, source);
+        hanoi_rec(n - 1, spare, target, source);
     }
 }
